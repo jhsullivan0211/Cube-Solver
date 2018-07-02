c9561dda9cd0cc41533ef2b6f31d8893bcf952ec
Joshua Sullivan <joshuasullivan0211@gmail.com>
2018-07-02 00:26:40 -0500

    Switched from pygame to Panda3d
    Switch from pygame to Panda3d, since pygame was proving pretty limited.
    Roughly got to the same place as before, with random colors put on each
    sticker, though the cube needs to be tidied up and a decent rotation put
    in.

--- user_interface.py
+++ user_interface.py
@@ -1,75 +1,113 @@
 """Contains the classes needed to build and run the UI."""
-import pygame
+
 import random
-from pygame.locals import *
+
 import math
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+from direct.showbase.ShowBase import ShowBase
+from direct.showbase.DirectObject import DirectObject
+from direct.gui.DirectGui import *
+from direct.interval.IntervalGlobal import *
+from panda3d.core import lookAt
+from panda3d.core import GeomVertexFormat, GeomVertexData
+from panda3d.core import Geom, GeomTriangles, GeomVertexWriter
+from panda3d.core import Texture, GeomNode
+from panda3d.core import PerspectiveLens
+from panda3d.core import CardMaker
+from panda3d.core import Light, Spotlight
+from panda3d.core import TextNode
+from panda3d.core import LVector3
+from direct.task import Task
+import sys
+import os
 
-class CubeDrawer:
+
+class CubeModel:
     """Contains data and methods for drawing a cube."""
     stickers = None
     color_map = {}
+    cube_model = None
+    cube_rotation = (0, 0, 0)
 
     def __init__(self):
         """Constructor for CubeDrawer.  Generates all of the points of the cube and stores them
-        for quick drawing, as well as a map of each sticker to its proper color.  NOTE: the name
-        'sticker' here may be somewhat misleading, because it is actually the entire square space
-        around and including the sticker on a real cube."""
+        for quick drawing, as well as a map of each sticker to its proper color."""
 
-        self.stickers = CubeDrawer.generate_stickers()
+        self.stickers = CubeModel.generate_stickers()
         for sticker in self.stickers:
             red = random.random()
             green = random.random()
             blue = random.random()
             self.color_map[sticker] = tuple([red, green, blue])
-            print(self.color_map)
 
+        self.draw_cube()
 
     def draw_cube(self):
         """Draws the cube using the stickers built during construction."""
+
+
+        geom_node = GeomNode('gnode')
         for sticker in self.stickers:
-            self.draw_sticker(sticker, self.color_map[sticker])
+            sticker_geom = self.draw_sticker(sticker, self.color_map[sticker])
+            geom_node.addGeom(sticker_geom)
+
+        node_path = render.attachNewNode(geom_node)
+        node_path.setTwoSided(True)
+        self.cube_model = node_path
+        return node_path
+
 
     @staticmethod
     def draw_sticker(sticker, sticker_color):
-        """Draws a single sticker the specified sticker color."""
-        glBegin(GL_TRIANGLES)
 
-        glColor(sticker_color)
-        glVertex(sticker[0])
-        glVertex(sticker[2])
-        glVertex(sticker[3])
+        vertex_format = GeomVertexFormat.getV3c4()
+        vertex_data = GeomVertexData("vertices", vertex_format, Geom.UHStatic)
+        vertex_data.setNumRows(4)
 
-        glVertex(sticker[1])
-        glVertex(sticker[2])
-        glVertex(sticker[3])
+        vertex_writer = GeomVertexWriter(vertex_data, 'vertex')
+        color_writer = GeomVertexWriter(vertex_data, 'color')
 
-        # glColor(255, 0, 0)
-        # glVertex(colored_part[0])
-        # glVertex(colored_part[2])
-        # glVertex(colored_part[3])
-        #
-        # glVertex(colored_part[1])
-        # glVertex(colored_part[2])
-        # glVertex(colored_part[3])
+        vertex_writer.addData3f(sticker[0])
+        color_writer.addData3f(sticker_color)
 
-        glEnd()
+        vertex_writer.addData3f(sticker[1])
+        color_writer.addData3f(sticker_color)
+
+        vertex_writer.addData3f(sticker[2])
+        color_writer.addData3f(sticker_color)
+
+        vertex_writer.addData3f(sticker[3])
+        color_writer.addData3f(sticker_color)
+
+        color_writer.addData3f(sticker_color)
+
+        prim = GeomTriangles(Geom.UH_static)
+        prim.addVertices(0, 1, 2)
+        prim.addVertices(0, 3, 1)
+
+        prim.closePrimitive()
+
+        geom = Geom(vertex_data)
+        geom.addPrimitive(prim)
+
+        return geom
+
+    def rotate(self, rotation):
+        new_rotation = tuple(map(sum, zip(rotation, self.cube_rotation)))
+        self.cube_rotation = new_rotation
+        self.cube_model.setHpr(self.cube_rotation)
+
 
     @staticmethod
-    def scale_square(square, scalar):
-        """Returns a square scaled by the specified scalar such that the center remains the same."""
+    def scale_sticker(sticker, scalar):
         result = []
-        for point in square:
-            scaled = []
-            for component in point:
-                scaled_val = component * scalar
-                scaled.append(scaled_val)
+        for vertex in sticker:
+            scaled = [point * scalar for point in vertex]
             result.append(tuple(scaled))
-        return tuple(result)
-
+        return result
 
     @staticmethod
     def generate_stickers():
@@ -78,14 +116,14 @@ class CubeDrawer:
 
         stickers = []
         edge_to_mid = {}
-        middles = CubeDrawer.branch((0, 0, 0))
+        middles = CubeModel.branch((0, 0, 0))
 
         for middle in middles:
             corner_to_edge = {}
-            edges = CubeDrawer.branch(middle)
+            edges = CubeModel.branch(middle)
             for edge in edges:
                 edge_to_mid[edge] = middle
-                corners = CubeDrawer.branch(edge)
+                corners = CubeModel.branch(edge)
                 for corner in corners:
                     if corner in corner_to_edge:
                         sticker = tuple([middle, corner, edge, corner_to_edge[corner]])
@@ -98,9 +136,9 @@ class CubeDrawer:
     @staticmethod
     def branch(point):
         """Given a vertex on the cube, gets all points that can be reached by advancing (i.e. moving
-        away from zero) in a single axis direction.  This functions in such a way that, by calling this
-        on the center point, it generates middle points; from middle points, it generates edge points;
-        and from edge points, it generates corner points."""
+        away from zero) in a single axis direction.  This functions in such a way that by calling this
+        on the center point it generates middle points, from a middle point it generates the four edge
+        points on the same face, and from an edge point it generates the two adjacent corner points."""
 
         result = []
         for i in range(0, 3):
@@ -113,86 +151,48 @@ class CubeDrawer:
                 result.append((tuple(copy2)))
         return result
 
-
-def main():
-    """The main entry to the program, which loads the GUI and runs the main loop."""
-
-    # Initialization
-    pygame.init()
-    display = (800, 600)
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -5)
-    glEnable(GL_DEPTH_TEST)
-
-    drawer = CubeDrawer()
-    rotate_x = False
-    rotate_y = False
-    x_angle = 0
-    y_angle = 0
-    z_angle = 0
-
-    # Main loop, executed every tick:
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    rotate_y = True
-                if event.button == 3:
-                    rotate_x = True
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    rotate_y = False
-                if event.button == 3:
-                    rotate_x = False
-
-        message = ""
-
-        if rotate_x:
-            # x_factor = math.cos(math.radians(y_angle))
-            # y_factor = math.sin(math.radians(z_angle))
-            # z_factor = math.sin(math.radians(y_angle))
-
-            x_factor = 1
-            y_factor = 0
-            z_factor = 0
-
-            glRotate(1, x_factor, y_factor, z_factor)
-
-            x_angle += x_factor
-            y_angle += y_factor
-            z_angle += z_factor
-
-            if x_angle == 360:
-                x_angle = 0
-            #print(str(x_angle) + ", " + str(y_angle))
-
-        if rotate_y:
-            # x_factor = math.sin(math.radians(z_angle))
-            # y_factor = math.cos(math.radians(x_angle))
-            # z_factor = math.sin(math.radians(x_angle))
-
-            x_factor = 0
-            y_factor = 1
-            z_factor = 1
-
-            glRotate(1, x_factor, y_factor, z_factor)
-
-            x_angle += x_factor
-            y_angle += y_factor
-            z_angle += z_factor
-
-            if y_angle == 360:
-                y_angle = 0
-            #print(str(x_angle) + ", " + str(y_angle) + ", " + str(z_angle))
-
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        drawer.draw_cube()
-        pygame.display.flip()
-        pygame.time.wait(10)
+    def test(self):
+        print(self.stickers)
 
 
-main()
+class MyApp(ShowBase):
+
+
+    key_a = False
+
+
+
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.camera.setPos(0, -10, 0)
+        self.disableMouse()
+        self.camera.setPos(0, -10, 0)
+        self.accept('a', self.rotate_heading_neg)
+        self.accept('s', self.rotate_pitch_pos)
+        self.accept('d', self.rotate_heading_pos)
+        self.accept('w', self.rotate_pitch_neg)
+
+        self.cube = CubeModel()
+
+    def rotate_heading_pos(self):
+        self.cube.rotate((10, 0, 0))
+
+    def rotate_heading_neg(self):
+        self.cube.rotate((-10, 0, 0))
+
+    def rotate_pitch_neg(self):
+        self.cube.rotate((0, -10, 0))
+
+    def rotate_pitch_pos(self):
+        self.cube.rotate((0, 10, 0))
+
+    def rotate(task):
+        self.cube.rotate((-1, 0, 0))
+
+
+
+
+
+
+app = MyApp()
+app.run()
